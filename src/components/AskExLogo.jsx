665c85270780876a598949e31791183915f5abc0
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-09-28 13:48:51 +0700

    make logo component

---
+++ src/components/AskExLogo.jsx
@@ -0,0 +1,12 @@
+import { Logo } from "../assets/assets";
+
+const AskExLogo = () => {
+  return (
+    <div className="flex items-center gap-4">
+      <img src={Logo} alt="ask experts logo" />
+      <h3 className="text-sec-white">AskExperts</h3>
+    </div>
+  );
+};
+
+export default AskExLogo;
