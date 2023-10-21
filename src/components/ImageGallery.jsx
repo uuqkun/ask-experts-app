fcc4de4aa2baf762cf93275423a8c50d7da645de
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-10-21 18:53:05 +0700

    responsive gallery

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,12 +1,13 @@
+import { gallery } from "../constants/Gallery";
 import { Button, HeaderSection } from "./components";
 
 const ImageGallery = () => {
   return (
     <section className="flex justify-center mb-40 relative pt-20">
-      <div className="absolute top-0 left-0 z-[-1] w-full h-[345px] bg-pri-dark-gray" />
+      <div className="bg-gallery absolute top-0 left-0 z-[-1] w-full h-[445px]" />
 
       <div className="lg:w-[1210px] w-[85%]">
-        <div className="flex justify-between items-end">
+        <div className="flex justify-between items-end mb-14">
           <HeaderSection
             {...{
               alignContent: "left",
@@ -19,6 +20,14 @@ const ImageGallery = () => {
           />
           <Button text="More" variations="btn-yellow" addClass="btn-base-pad"/>
         </div>
+
+        <ul className="flex flex-wrap justify-center lg:gap-10 gap-6 lg:bg-white py-10 px-4 rounded-[28px] lg:shadow-xl">
+            {gallery.map(pict => (
+                <li className="lg:w-[366px] w-[260px] lg:h-[344px] h-[260px] lg:shadow-none shadow-lg rounded-3xl overflow-hidden transition-all hover:scale-105 hover:shadow-xl">
+                    <img src={pict.icon} alt={pict.alt} className="object-cover w-full h-full"/>
+                </li>
+            ))}
+        </ul>
       </div>
     </section>
   );
