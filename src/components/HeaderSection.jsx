a653ef269177f9a099fde5a8f5c94d980ba3092e
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-10-21 17:40:59 +0700

    add custom color prop

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -5,24 +5,25 @@ const HeaderSection = ({
   tagText = "Empty",
   tagVariant = "green2",
   content = "Empty h2 tag",
+  color = "text-black"
 }) => {
   let isRight = alignContent.toLowerCase() === "right";
   let isCenter = alignContent.toLowerCase() === "center";
 
   return isCenter ? (
-    <header className={`max-w-[704px] flex flex-col mx-auto items-center`}>
+    <header className={`max-w-[704px] flex flex-col mx-auto items-center ${color}`}>
       <SectionTag text={tagText} variants={tagVariant} />
-      <h2 className={`text-center mb-10`}>{content}</h2>
+      <h2 className={`text-center`}>{content}</h2>
     </header>
   ) : isRight ? (
-    <header className={`max-w-[704px] flex flex-col items-end`}>
+    <header className={`max-w-[704px] flex flex-col items-end ${color}`}>
       <SectionTag text={tagText} variants={tagVariant} />
-      <h2 className={`text-right mb-10`}>{content}</h2>
+      <h2 className={`text-right`}>{content}</h2>
     </header>
   ) : (
-    <header className={`max-w-[704px] flex flex-col`}>
+    <header className={`max-w-[704px] flex flex-col ${color}`}>
       <SectionTag text={tagText} variants={tagVariant} />
-      <h2 className={`text-left mb-10`}>{content}</h2>
+      <h2 className={`text-left`}>{content}</h2>
     </header>
   );
 };
