431266a9536815333597e672d9fbf6d734dd8a85
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-10-22 15:41:35 +0700

    Responsive gallery section

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -4,10 +4,10 @@ import { Button, HeaderSection } from "./components";
 const ImageGallery = () => {
   return (
     <section className="flex justify-center mb-40 relative pt-20">
-      <div className="bg-gallery absolute top-0 left-0 z-[-1] w-full h-[445px]" />
+      <div className="bg-gallery absolute top-0 left-0 z-[-1] w-full lg:h-[445px] sm:h-[50%] h-[70%]" />
 
       <div className="lg:w-[1210px] w-[85%]">
-        <div className="flex justify-between items-end mb-14">
+        <div className="flex sm:flex-row flex-col justify-between sm:items-end items-start mb-14 gap-10">
           <HeaderSection
             {...{
               alignContent: "left",
@@ -15,19 +15,25 @@ const ImageGallery = () => {
               tagVariant: "green1",
               content:
                 "Our client, global analytical techno company, wanted to build market.",
-                color: "text-white"
+              color: "text-white",
             }}
           />
-          <Button text="More" variations="btn-yellow" addClass="btn-base-pad"/>
+          <Button text="More" variations="btn-yellow" addClass="btn-base-pad" />
         </div>
 
-        <ul className="flex sm:flex-wrap flex-nowrap sm:justify-center sm:w-full overflow-x-scroll w-[300%] lg:gap-10 gap-6 lg:bg-white py-10 px-4 rounded-[28px] lg:shadow-xl ">
-            {gallery.map(pict => (
-                <li className="lg:w-[366px] w-[260px] lg:h-[344px] h-[260px] lg:shadow-none shadow-lg rounded-3xl overflow-hidden transition-all hover:scale-105 hover:shadow-xl">
-                    <img src={pict.icon} alt={pict.alt} className="object-cover w-full h-full"/>
-                </li>
+        <div className="w-full overflow-x-scroll">
+          <ul className="flex sm:flex-wrap sm:justify-center justify-start sm:w-full min-w-fit lg:gap-10 gap-6 sm:bg-white py-10 px-4 rounded-[28px] lg:shadow-xl">
+            {gallery.map((pict) => (
+              <li className="lg:w-[366px] w-[260px] lg:h-[344px] h-[260px] lg:shadow-none shadow-lg rounded-3xl overflow-hidden transition-all hover:scale-105 hover:shadow-xl">
+                <img
+                  src={pict.icon}
+                  alt={pict.alt}
+                  className="object-cover w-full h-full"
+                />
+              </li>
             ))}
-        </ul>
+          </ul>
+        </div>
       </div>
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,6 @@ const Home = () => {
       <Process />
 
       <ImageGallery/>
-
       
       
     </div>
