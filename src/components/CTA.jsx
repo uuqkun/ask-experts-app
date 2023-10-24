943e19383ad3d564b496a53f087ce29d4240f2ca
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-10-24 13:23:59 +0700

    initiated

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const CTA = () => {
   return (
-    <div>
-      <h2>CTA</h2>
-    </div>
+    <section className="flex justify-center mb-10 relative pt-20">
+      <div className="lg:w-[1210px] w-[85%]">
+        <h2>CTA</h2>
+      </div>
+    </section>
   );
 };
 
