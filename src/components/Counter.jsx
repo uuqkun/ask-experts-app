a07c0252816da551793be1700c6935cd08c78fe2
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-10-24 07:16:35 +0700

    test

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,6 +9,7 @@ const Counter = () => {
             <li key={data.title}>
               <h1 className="w-fit mx-auto">{data.count}+</h1>
               <p className="text-3xl">{data.title}</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sed, eveniet aliquid velit iure natus excepturi? Exercitationem molestiae dicta magnam illum blanditiis nam veniam! Explicabo voluptate eaque cum reprehenderit consectetur, quaerat ea sit impedit. Voluptas vero quidem, possimus illo vitae id nulla facere obcaecati excepturi. Aperiam ducimus at, animi rem delectus maxime totam, obcaecati fuga nisi quia dolore recusandae nam quidem ipsa a. Incidunt consequuntur laudantium ex, reprehenderit non nam cum quia, dignissimos expedita laborum cupiditate provident unde doloremque ut, nostrum omnis sequi repudiandae? Ducimus impedit nisi voluptates veritatis porro, sit vero nesciunt excepturi nam laborum corrupti assumenda? Est, mollitia.</p>
             </li>
           ))}
         </ul>
